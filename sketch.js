b3ffe03bebc16e7de61574be54aea40b867b482d
Italo Adler <adler.italo@gmail.com>
2017-05-15 03:15:48 -0300

    a big screen emerge in front of you

--- sketch.js
+++ sketch.js
@@ -1,70 +1,21 @@
-var system;
-
 function setup() {
-  createCanvas(displayWidth,displayHeight);
-  background(255,127,111);
-  system = new ParticleSystem(createVector(width/2, 50));
+  createCanvas(displayWidth, displayHeight);
+  pixelDensity(1);
 }
-
 
 function draw() {
-  background(255,127,111);
-  system.addParticle();
-  system.run();
-}
+  background(51);
 
-// A simple Particle class
-var Particle = function(position) {
-  this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-1, 1), random(-1, 0));
-  this.position = position.copy();
-  this.lifespan = 255.0;
-};
-
-Particle.prototype.run = function() {
-  this.update();
-  this.display();
-};
-
-// Method to update position
-Particle.prototype.update = function(){
-  this.velocity.add(this.acceleration);
-  this.position.add(this.velocity);
-  this.lifespan -= 2;
-};
-
-// Method to display
-Particle.prototype.display = function() {
-  stroke(220, this.lifespan);
-  strokeWeight(0.5);
-  fill(127, this.lifespan);
-  point(this.position.x, this.position.y);
-};
-
-// Is the particle still useful?
-Particle.prototype.isDead = function(){
-  if (this.lifespan < 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-var ParticleSystem = function(position) {
-  this.origin = position.copy();
-  this.particles = [];
-};
-
-ParticleSystem.prototype.addParticle = function() {
-  this.particles.push(new Particle(this.origin));
-};
-
-ParticleSystem.prototype.run = function() {
-  for (var i = this.particles.length-1; i >= 0; i--) {
-    var p = this.particles[i];
-    p.run();
-    if (p.isDead()) {
-      this.particles.splice(i, 1);
+  loadPixels();
+  for (var y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      var index = (x + y * width)*4;
+      pixels[index+0] = x;
+      pixels[index+1] = random(255) ;
+      pixels[index+2] = y;
+      pixels[index+3] = 255;
     }
   }
-};
+  updatePixels();
+
+}
