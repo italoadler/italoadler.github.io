ad781f859ee4762380432f80bc4c7f500a18e86f
Italo Adler <adler.italo@gmail.com>
2017-11-22 01:24:34 -0300

    pense sobre o seu papel no mundo, o que te faz difente de uma planta ou um boi?

--- sketch.js
+++ sketch.js
@@ -1,21 +1,50 @@
+/*
+  @italoadler 2017
+  sound and digital artist.
+  ruido_branco_ruido_rosa, é parte da série lamentos de uma queimada;
+*/
+
+var ruido, invertido;
+
+function preload() {
+  background(255,100,100);
+
+  ruido = loadSound('material/rrrrr_menor.wav');
+
+}
+
 function setup() {
   createCanvas(displayWidth, displayHeight);
-  pixelDensity(1);
+  ruido.setVolume(0.1);
+  ruido.play();
 }
 
 function draw() {
-  background(51);
-
+  background(255,0,0);
+  noStroke();
+  fill(255,100,100,map(mouseX,0,255,0,width));
+  rect(0,0,width,height);
+  ruido.play();
   loadPixels();
-  for (var y = 0; y < height; y++) {
-    for (var x = 0; x < width; x++) {
-      var index = (x + y * width)*4;
-      pixels[index+0] = x;
-      pixels[index+1] = random(255) ;
-      pixels[index+2] = y;
-      pixels[index+3] = 255;
+  if(mouseIsPressed) {
+    background(255,random(110,140),random(120,130));
+    ruido.reverseBuffer();
+    ruido.play();
+
+    pixelDensity(random(0.1,1) );
+
+  } else {
+    for (var y = 0; y < height; y++) {
+      for (var x = 0; x < width; x++) {
+        var index = (x + y * width)*4;
+        pixels[index+0] = x;
+        pixels[index+1] = random(255) ;
+        pixels[index+2] = y;
+        pixels[index+3] = 255;
+      }
     }
   }
+
   updatePixels();
 
 }
